a7f463f21d1a1887f141910adcbfc4bef9b7533b
Mu Ye <emailyemu@gmail.com>
2025-06-21 10:15:35 -0700

    2024: day 3.2

--- 2024/day3/main.go
+++ 2024/day3/main.go
@@ -31,8 +31,37 @@ func part1(lines []string) int {
 	return total
 }
 
+func part2(lines []string) int {
+	text := strings.Join(lines, "")
+	r := regexp.MustCompile(`mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)`)
+	tokens := r.FindAllString(text, -1)
+	total := 0
+	mode := true // true=do, false=don't
+	for _, token := range tokens {
+		switch token {
+		case "do()":
+			mode = true
+		case "don't()":
+			mode = false
+		default:
+			if mode {
+				token = strings.TrimPrefix(token, "mul(")
+				token = strings.TrimSuffix(token, ")")
+				parts := strings.Split(token, ",")
+				num1, _ := strconv.Atoi(parts[0])
+				num2, _ := strconv.Atoi(parts[1])
+				total += num1 * num2
+			}
+		}
+	}
+	return total
+}
+
 func main() {
 	lines := utils.ReadLines("input.txt")
 	res1 := part1(lines)
 	fmt.Printf("Part 1: %d\n", res1)
+
+	res2 := part2(lines)
+	fmt.Printf("Part 2: %d\n", res2)
 }
