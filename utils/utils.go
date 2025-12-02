8af0048641ba96ad2ea960b3781bc628352843f7
Mu Ye <emailyemu@gmail.com>
2025-12-01 21:21:24 -0800

    Refactor utils

--- utils/utils.go
+++ utils/utils.go
@@ -5,17 +5,21 @@ import (
 	"strings"
 )
 
-func ReadLines(filename string) []string {
-	dat, err := os.ReadFile(filename)
-	if err != nil {
-		panic("error reading the file")
-	}
-	return strings.Split(string(dat), "\n")
-}
-
 func AbsInt(x int) int {
 	if x < 0 {
 		return -x
 	}
 	return x
+}
+
+func ReadLines(filename string) []string {
+	return readInput(filename, "\n")
+}
+
+func readInput(filename, delimiter string) []string {
+	dat, err := os.ReadFile(filename)
+	if err != nil {
+		panic("error reading the file")
+	}
+	return strings.Split(string(dat), delimiter)
 }
