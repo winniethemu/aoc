e8ad6e0c5bc8e9c48f83c3f96dba744efb65ec52
Mu Ye <emailyemu@gmail.com>
2025-12-02 21:57:06 -0800

    2025: day 3.2

--- 2025/day3/main.go
+++ 2025/day3/main.go
@@ -31,9 +31,28 @@ func part1(banks []string) int {
 	return res
 }
 
+func part2(banks []string, size int) int {
+	res := 0
+	for _, bank := range banks {
+		batteries := strings.Split(bank, "")
+		sum := 0
+		for sz := size - 1; sz >= 0; sz-- {
+			battery, i := findMax(batteries[:len(batteries)-sz])
+			sum *= 10
+			sum += battery
+			batteries = batteries[i+1:]
+		}
+		res += sum
+	}
+	return res
+}
+
 func main() {
 	lines := utils.ReadLines("input.txt")
 
 	res1 := part1(lines)
 	fmt.Printf("Part 1: %d\n", res1)
+
+	res2 := part2(lines, 12)
+	fmt.Printf("Part 2: %d\n", res2)
 }
