aa3e085a07c85fa9f5591a31212805974802ddaa
Mu Ye <emailyemu@gmail.com>
2025-12-03 22:15:50 -0800

    2025: day 4.2

--- 2025/day4/main.go
+++ 2025/day4/main.go
@@ -6,50 +6,84 @@ import (
 	"strings"
 )
 
+var neighbors = [8][2]int{
+	{-1, -1}, {-1, 0}, {-1, 1},
+	{0, -1}, {0, 1},
+	{1, -1}, {1, 0}, {1, 1},
+}
+
+func inBounds(mat [][]string, row, col int) bool {
+	return row >= 0 && row < len(mat) && col >= 0 && col < len(mat[0])
+}
+
 func accessible(mat [][]string, row, col int) bool {
-	n, m := len(mat), len(mat[0])
-	neighbors := [8][2]int{
-		{-1, -1},
-		{-1, 0},
-		{-1, 1},
-		{0, -1},
-		{0, 1},
-		{1, -1},
-		{1, 0},
-		{1, 1},
-	}
 	count := 0
 	for _, neighbor := range neighbors {
-		dr, dc := neighbor[0], neighbor[1]
-		if 0 <= dr+row && dr+row < n && 0 <= dc+col && dc+col < m {
-			if mat[dr+row][dc+col] == "@" {
-				count++
-			}
+		nr, nc := row+neighbor[0], col+neighbor[1]
+		if inBounds(mat, nr, nc) && mat[nr][nc] == "@" {
+			count++
 		}
 	}
 	return count < 4
 }
 
-func part1(mat [][]string) int {
-	res := 0
-	n, m := len(mat), len(mat[0])
-	for i := range n {
-		for j := range m {
-			if mat[i][j] == "@" && accessible(mat, i, j) {
-				res++
+func countRolls(mat [][]string) int {
+	count := 0
+	for i := range len(mat) {
+		for j := range len(mat[0]) {
+			if mat[i][j] == "@" {
+				count++
 			}
 		}
 	}
-	return res
+	return count
+}
+
+// returns all accessible locations
+func part1(mat [][]string) [][2]int {
+	var locations [][2]int
+	for i := range len(mat) {
+		for j := range len(mat[0]) {
+			if mat[i][j] == "@" && accessible(mat, i, j) {
+				locations = append(locations, [2]int{i, j})
+			}
+		}
+	}
+	return locations
+}
+
+func part2(mat [][]string) int {
+	before := countRolls(mat)
+	queue := part1(mat)
+
+	for len(queue) > 0 {
+		row, col := queue[0][0], queue[0][1]
+		queue = queue[1:]
+		mat[row][col] = "."
+
+		for _, neighbor := range neighbors {
+			nr, nc := row+neighbor[0], col+neighbor[1]
+			if inBounds(mat, nr, nc) && mat[nr][nc] == "@" && accessible(mat, nr, nc) {
+				queue = append(queue, [2]int{nr, nc})
+			}
+		}
+	}
+
+	after := countRolls(mat)
+	return before - after
 }
 
 func main() {
 	lines := utils.ReadLines("input.txt")
 	mat := make([][]string, len(lines))
-	for i := 0; i < len(lines); i++ {
-		chars := strings.Split(lines[i], "")
-		mat[i] = chars
+	for i, line := range lines {
+		mat[i] = strings.Split(line, "")
 	}
-	res1 := part1(mat)
+
+	locations := part1(mat)
+	res1 := len(locations)
 	fmt.Printf("Part 1: %d\n", res1)
+
+	res2 := part2(mat)
+	fmt.Printf("Part 2: %d\n", res2)
 }
