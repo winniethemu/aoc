d7885ca0c79b685721e0820031ab5b4260bf5bf7
Mu Ye <emailyemu@gmail.com>
2025-12-06 23:19:42 -0800

    2025: day 7.2

--- 2025/day7/main.go
+++ 2025/day7/main.go
@@ -37,50 +37,44 @@ func part1(manifold [][]string) int {
 }
 
 func part2(manifold [][]string) int {
-	res := 0
 	n, m := len(manifold), len(manifold[0])
 
-	dp := make([][]int, 0)
+	dp := make([][]int, n)
 	for i := range n {
-		dp = append(dp, make([]int, m))
-		for j := range m {
-			dp[i][j] = 0
-		}
+		dp[i] = make([]int, m)
 	}
 
-	r0, c0 := 0, slices.Index(manifold[0], "S")
+	// start position has 1 timeline
+	src := slices.Index(manifold[0], "S")
+	dp[0][src] = 1
 
-	queue := make([][2]int, 0)
-	queue = append(queue, [2]int{r0, c0})
-	for len(queue) > 0 {
-		node := queue[0]
-		queue = queue[1:]
-		r, c := node[0], node[1]
-		if r < 0 || r > n-1 || c < 0 || c > m-1 {
-			continue
-		}
-		switch manifold[r][c] {
-		case "^":
-			queue = append(queue, [2]int{r, c - 1}, [2]int{r, c + 1})
-		case "S":
-			dp[r][c] = 1
-			queue = append(queue, [2]int{r + 1, c})
-		case ".":
-			dp[r][c] = dp[r-1][c]
-			if c > 0 && manifold[r][c-1] == "^" {
-				dp[r][c] += dp[r-1][c-1]
+	for r := range n {
+		// first pass: handle splitters
+		for c := range m {
+			if manifold[r][c] == "^" && dp[r][c] > 0 {
+				// split timelines to left and right on the same row
+				if c > 0 {
+					dp[r][c-1] += dp[r][c]
+				}
+				if c < m-1 {
+					dp[r][c+1] += dp[r][c]
+				}
+				// clear the splitter cell - timelines have been split
+				dp[r][c] = 0
 			}
-			if c < m-1 && manifold[r][c+1] == "^" {
-				dp[r][c] += dp[r-1][c+1]
+		}
+
+		// second pass: propagate all timelines downward
+		if r < n-1 {
+			for c := range m {
+				if dp[r][c] > 0 {
+					dp[r+1][c] += dp[r][c]
+				}
 			}
-			queue = append(queue, [2]int{r + 1, c})
 		}
 	}
 
-	for i := range n {
-		fmt.Println(dp[i])
-	}
-
+	res := 0
 	for i := range m {
 		res += dp[n-1][i]
 	}
@@ -98,7 +92,7 @@ func parse(lines []string) [][]string {
 }
 
 func main() {
-	lines := utils.ReadLines("example.txt")
+	lines := utils.ReadLines("input.txt")
 	manifold := parse(lines)
 
 	res1 := part1(manifold)
